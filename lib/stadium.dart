713b1828aafaaa2774b29844bb4a65f34241c949
NomadHuns <kimalss22@gmail.com>
2023-05-01 15:39:31 +0900

    공통된 부분만 받기 Test

---
+++ lib/stadium.dart
@@ -0,0 +1,32 @@
+import 'package:different_type_parse/source_file.dart';
+
+class Stadium {
+  final int id;
+  final String name;
+  final SourceFile sourceFile;
+
+  Stadium({
+    required this.id,
+    required this.name,
+    required this.sourceFile,
+  });
+
+  // 통신을 위해서 json 처럼 생긴 문자열 {"id":1} => Dart 오브젝트
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['id'] = id;
+    data['name'] = name;
+    data['sourceFile'] = sourceFile;
+    return data;
+  }
+
+  factory Stadium.fromJson(Map<String, dynamic> json) {
+    return Stadium(
+      id: json['id'],
+      name: json['name'],
+      sourceFile: SourceFile.fromJson(json['sourceFile']),
+    );
+  }
+
+}
---
+++ test/stadium_repository_test.dart
@@ -0,0 +1,38 @@
+import 'package:different_type_parse/my_dio.dart';
+import 'package:different_type_parse/response_dto.dart';
+import 'package:different_type_parse/stadium.dart';
+import 'package:dio/dio.dart';
+import 'package:logger/logger.dart';
+
+String token = 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJqd3RzdHVkeSIsInJvbGUiOiJQTEFZRVIiLCJuaWNrbmFtZSI6InNzYXIxMjMiLCJpZCI6MSwiZXhwIjoxNjgzMDA5MTE4fQ.NczIkH07fPyUAwrF5Tm1BDGCiIa7tk3D9-QbbUGKuNeSb0iS_QbKW3gyUClxVEvf5hYGVhv8ADimpE46-9TOcw';
+
+void main() async {
+  await fetchStadiumList_test();
+  await fetchStadiumDetail_test();
+}
+
+Future<void> fetchStadiumList_test() async {
+  String jwt = token;
+  String keyword = '야구';
+  Response response = await dio.get("/api/user/stadiums?keyword="+keyword,
+      options: Options(headers: {"Authorization": "$jwt"}));
+  ResponseDTO responseDTO = ResponseDTO.fromJson(response.data);
+  Logger().d(responseDTO.data);
+  List<dynamic> mapList = responseDTO.data as List<dynamic>;
+  List<Stadium> postList = mapList.map((e) => Stadium.fromJson(e)).toList();
+  responseDTO.data = postList;
+  Logger().d(postList[0].id);
+  Logger().d(postList[0].name);
+}
+
+Future<void> fetchStadiumDetail_test() async {
+  String jwt = token;
+  int stadiumId = 1;
+  Response response = await dio.get("/api/user/stadium/$stadiumId",
+      options: Options(headers: {"Authorization": "$jwt"}));
+
+  ResponseDTO responseDTO = ResponseDTO.fromJson(response.data);
+  responseDTO.data = Stadium.fromJson(responseDTO.data);
+
+  Logger().d(response.data);
+}
